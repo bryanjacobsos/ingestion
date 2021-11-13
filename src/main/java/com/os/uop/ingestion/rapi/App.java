919ab12f96db9d767d45e72744d30a80247ab85d
Bryan Jacobs <bryan.jacobs@outsystems.com>
2021-11-13 11:05:17 -0700

    more comments to clarify example

---
+++ src/main/java/com/os/uop/ingestion/rapi/App.java
@@ -0,0 +1,25 @@
+package com.os.uop.ingestion.rapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The below main method starts the rest api.
+ *
+ * @see com.os.uop.ingestion.rapi.rest.ExportLogsServiceRequestRest to see how the code flow works
+ */
+@SpringBootApplication
+public class App {
+
+    /**
+     * Starts the REST API listening on the configured port in the application.properties or
+     * overridden on the commandline
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        SpringApplication.run(App.class, args);
+
+    }
+
+}
